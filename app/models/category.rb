4ab5d4f44dc1343826e60d74a795c4ee23d2ab74
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 16:01:13 +0200

    Add 'main_category' scope to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,6 @@ class Category < ActiveRecord::Base
   has_many :subcategories, foreign_key: 'parent_id', class_name: 'Category'
 
   validates :parent_id, :name, presence: true
+
+  scope :main_category, ->  { where 'parent_id = ?', 0 }
 end
