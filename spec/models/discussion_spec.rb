87927e8e3d72f87c9d1e16af19aab3ea97882b71
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 13:55:50 +0200

    Prepare tests for ref to User in Discussion model

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 RSpec.describe Discussion do
   describe 'database columns' do
     it { is_expected.to have_db_column :product_id }
+    it { is_expected.to have_db_column :user_id }
   end
 
   describe 'validations' do
     it { is_expected.to validate_presence_of :product_id }
+    it { is_expected.to validate_presence_of :user_id}
   end
 
   describe 'associations' do
     it { is_expected.to belong_to :product }
+    it { is_expected.to belong_to :user }
   end
 end
