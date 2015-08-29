6727a0183b6f808d36a27dcd0fd0081acaaf081e
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 13:00:21 +0200

    Prepare tests for ref to User in ProductUser model

--- spec/models/product_user_spec.rb
+++ spec/models/product_user_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe ProductUser do
   describe 'database columns' do
     it { is_expected.to have_db_column :product_id }
+    it { is_expected.to have_db_column :user_id }
   end
 
   describe 'associations' do
     it { is_expected.to belong_to :product }
+    it { is_expected.to belong_to :user }
   end
 end
