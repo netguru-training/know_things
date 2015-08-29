e00a680843af6b71d0ef160b2f5bff8bf4520837
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 12:49:18 +0200

    Add Category ref to Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829101924) do
+ActiveRecord::Schema.define(version: 20150829104635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150829101924) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,4 +69,5 @@ ActiveRecord::Schema.define(version: 20150829101924) do
 
   add_foreign_key "discussions", "products"
   add_foreign_key "product_users", "products"
+  add_foreign_key "products", "categories"
 end
