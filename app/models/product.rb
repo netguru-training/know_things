0b71bbeb56cbf28afac9a1efa1eeac140b3229b5
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 11:47:53 +0200

    Add association has many product_users to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :product_users
+
   validates :name, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Product do
   describe 'validations' do
     it { is_expected.to validate_presence_of :name }
   end
+
+  describe 'associations' do
+    it { is_expected.to have_many :product_users }
+  end
 end
