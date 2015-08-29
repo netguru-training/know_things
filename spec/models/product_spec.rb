91d97895c653958754e01ca139f60c4cbb9d2e26
Lukasz Maslej <lukasz.maslej@rubycoder.pl>
2015-08-29 11:55:59 +0200

    Refactor models spec, use expect syntax

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Product do
   describe 'database columns' do
-    it { should have_db_column :name }
-    it { should have_db_column :description }
+    it { is_expected.to have_db_column :name }
+    it { is_expected.to have_db_column :description }
   end
 
   describe 'validations' do
--- spec/models/product_user_spec.rb
+++ spec/models/product_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ProductUser do
   describe 'database columns' do
-    it { should have_db_column :product_id }
+    it { is_expected.to have_db_column :product_id }
   end
 
   describe 'associations' do
