1721248c210e11031bdba2ab90bd09be6e8f7d07
kacper331 <denebv201@gmail.com>
2015-08-29 17:23:05 +0200

    Create method 'new' in Registrations controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
   end
+
+  def new
+    @categories = Category.all.includes(:subcategories)
+    super
+  end
 end
