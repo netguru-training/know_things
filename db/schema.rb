248fff8d29855f60973e02c1a48f000c3a7d146d
konto-andrzeja <konto.andrzeja@gmail.com>
2015-08-30 10:01:22 +0200

    remove description from products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829120762) do
+ActiveRecord::Schema.define(version: 20150830075746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20150829120762) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
