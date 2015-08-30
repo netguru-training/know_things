f68c2b6989f14b7ffb28dd03de5ab8ec1faa0e85
kacper331 <denebv201@gmail.com>
2015-08-30 12:47:14 +0200

    Update add method in ProductsController of new variables

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
   def add
+    @products = Product.new()
+    @category = Category.all.includes(:subcategories)
+    @product = Product.all.order(:name)
+    @subcategories = Category.all.where(parent_id: 0)
+  end
+
   def get_list
     respond_to do |format|
       format.json { render json: { results: Product.all } }
