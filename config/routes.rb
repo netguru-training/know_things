187a709078458f7628485d0ba2fbe1a5dc7fbda1
konto-andrzeja <konto.andrzeja@gmail.com>
2015-08-30 14:51:40 +0200

    fix new_discussion_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'products/get_list'
   post 'products/create'
 
-  get 'discussions/new'
   get 'discussions/mine'
   devise_for :users, controllers: {registrations: 'registrations'}
   get 'visitors/index'
