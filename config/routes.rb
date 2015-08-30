36456c77147cc220740e65ec639f76d951467cd0
kacper331 <denebv201@gmail.com>
2015-08-30 15:38:17 +0200

    Add Messages Controller with new haml page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'visitors/index'
   resources :categories, only: :index
   resources :discussions, only: [:index, :new, :create, :show]
+  resources :messages, only: [:new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
